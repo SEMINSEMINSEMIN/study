a64ceff1a7af2d7d1d619bc0705a9cf929f230ad
강세민 <ksm9802@gmail.com>
2023-06-20 08:59:05 +0900

    webcomponents - shadow dom 접근성 테스트

--- js/customElement/ShadowClose.js
+++ js/customElement/ShadowClose.js
@@ -7,10 +7,20 @@ class ShadowClose extends HTMLElement {
     this.#shadowRoot.innerHTML = `
       <style>
         button {
+          margin: 30px auto 10px;;
+          display: block;
           color: green;
         }
       </style>
       <button></button>
+      <a href="www.yahoo.com">야후로 이동</a>
+      <footer>
+        <ul>
+          <li>리스트 1</li>
+          <li>리스트 2</li>
+          <li>리스트 3</li>
+        </ul>
+      </footer>
     `;
   }
 
