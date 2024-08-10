5b320d6a8a7be8f6b1f8909b5f62c9063c29a39e
SEMINSEMINSEMIN <ksm9802@gmail.com>
2024-08-11 08:47:56 +0900

    [react typescript] course11
    - useState Type Assertion

---
+++ typescript/react-typescript-demo/src/components/state/User2.tsx
@@ -0,0 +1,26 @@
+// useState Type Assertion
+import { useState } from "react";
+
+type AuthUser = {
+    name: string;
+    email: string;
+};
+
+export const User = () => {
+    const [user, setUser] = useState<AuthUser>({} as AuthUser);
+
+    const handleLogin = () => {
+        setUser({
+            name: "Semin",
+            email: "ksm9802@gmail.com",
+        });
+    };
+
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
+        </div>
+    );
+};
