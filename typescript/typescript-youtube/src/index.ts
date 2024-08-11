7687c1fabd2d7786ebf00841aec04b7b8d714562
SEMINSEMINSEMIN <ksm9802@gmail.com>
2024-08-12 06:40:42 +0900

    [react-youtube] type inference & type: any

--- typescript/typescript-youtube/src/index.ts
+++ typescript/typescript-youtube/src/index.ts
@@ -1,1 +1,11 @@
-let age = 20;
+// Type inference
+let sales = 500;
+let course = "TypeScript";
+let isPublished = true;
+
+// type: any
+// 최대한 any 타입은 사용하는 것을 지양할 것
+let level;
+function render(document: any) {
+    console.log(document);
+}
