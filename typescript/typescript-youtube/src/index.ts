5916148d5a6c54fb3787d2b05b00452a7cf5c233
SEMINSEMINSEMIN <ksm9802@gmail.com>
2024-08-12 20:51:04 +0900

    [typescript-youtube] optional chaining

--- typescript/typescript-youtube/src/index.ts
+++ typescript/typescript-youtube/src/index.ts
@@ -111,3 +111,23 @@ function greet(name: string | null | undefined) {
 }
 
 greet(null);
+
+// Optional Chaining
+type Customer = {
+    birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthday: new Date() };
+}
+
+let customer = getCustomer(0);
+console.log(customer?.birthday?.getFullYear());
+
+// Optional Element Access Operator
+let customers: string[] | null = null;
+customers?.[0];
+
+// Optional Call
+let log: any = null;
+log?.("a");
