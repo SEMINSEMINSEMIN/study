3ea4130fb9ffdd1952bedb4a924c4f477e17d04b
SEMINSEMINSEMIN <ksm9802@gmail.com>
2024-08-20 05:11:56 +0900

    [react] Describint the UI - Your First Component

---
+++ react/learn-react/src/components/Gallery.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Profile } from "./Profile";
+
+export const Gallery = () => {
+    return (
+        <section>
+            <h1>먼작귀</h1>
+            <Profile />
+            <Profile />
+            <Profile />
+        </section>
+    );
+};
---
+++ react/learn-react/src/components/Profile.tsx
@@ -0,0 +1,9 @@
+export const Profile = () => {
+    // JSX: 자바스크립트 내에 마크업을 임베드할 수 있게 해준다.
+    return (
+        <img
+            src="https://i.namu.wiki/i/2CQlNh9gyUiO_vN1RUSQa8Yhc6ETkhtjCuizHp1AJhJAp1NxF1sNYlvj9en27T4khZzeyS-wM4dwiuOJJ2WA2bj52oQMDLW4YWRLtRAX4Q0q-G_L7wrTes0sPNYMJi9fDyiwhMP_EwDFdHz_X1vekw.webp"
+            alt="우사기"
+        />
+    );
+};
