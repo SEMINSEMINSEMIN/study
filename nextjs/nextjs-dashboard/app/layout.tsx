1414ca36215e421d1a4a2c24a4c24e769aeb4935
SEMINSEMINSEMIN <ksm9802@gmail.com>
2024-12-10 20:50:11 +0900

    [next-js-공식문서] ch.3

--- nextjs/nextjs-dashboard/app/layout.tsx
+++ nextjs/nextjs-dashboard/app/layout.tsx
@@ -1,13 +1,18 @@
 import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    /**
+     * By adding Inter to the <body> element, the font will be applied throughout your application.
+     * cf) 'antialiased': Tailwind class, smooths out the font.
+     */
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={`${inter.className} antialiased`}>{children}</body>
         </html>
     );
 }
---
+++ nextjs/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Lusitana } from "next/font/google";
+/**
+ * Next.js automatically optimizes fonts in the application when you use the next/font module.
+ * It downloads font files at "build time" and hosts them with your other "static assets."
+ * This means when a user visits your application, there are "no additional network requests for fonts" which would impact performance.
+ */
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
